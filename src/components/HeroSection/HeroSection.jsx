ddca6b385029a4666953a3a0445edcda6674bfdd
Admin <radhakrishnanr2699@gmail.com>
2024-11-26 17:16:39 +0530

    add hero video

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <text className="text-sm max-lg:hidden absolute left-0 bottom-52 text-slate-700 -rotate-90">Helping brands <br /> reach new heights</text>
         <h1 className="font-bold text-center max-w-4xl leading-[110%] tracking-tight text-5xl sm:text-7xl md:text-[90px]">Design, Develop, <span className="text-primary">Deliver</span> Outstanding Solutions</h1>
         <p className="text-center tracking-wide mt-7 text-slate-700 max-w-xl">We craft customized solutions for businesses, individuals, and anyone ready to elevate their digital presence.</p>
-        <video autoPlay src="../../../public/assets/hero-section.mp4" className="mt-7 rounded-xl" width="600" height="300" muted loop playsInline />
+        <video autoPlay src="/assets/hero-section.mp4" className="mt-7 rounded-xl" width="600" height="300" muted loop playsInline />
       </div>
     </div>
   )
